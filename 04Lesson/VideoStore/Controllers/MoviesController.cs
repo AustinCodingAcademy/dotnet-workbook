1b58a3549cb29d623abaf4a2ea91cd94a377f3aa
CLofton <lofton.chris@gmail.com>
2016-11-17 21:00:08 -0600

    Update MoviesController.cs

--- 04Lesson/VideoStore/Controllers/MoviesController.cs
+++ 04Lesson/VideoStore/Controllers/MoviesController.cs
@@ -26,59 +26,5 @@ namespace VideoStore.Controllers
                 new Movie { Id = 2, Name = "Wall-e" }
             };
         }
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Customer 1" },
-                new Customer { Name = "Customer 2" }
-            };
-
-            var viewModel = new RandomMovieViewModel()
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
-        }
-
-
-        [Route("movies/released/{year}/{month:regex(\\d{4}:range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
-        
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-        // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-        */
     }
 }
