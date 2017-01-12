f8ad8937d3f3b7f67be06ebe1f19a23fc6019a2a
Erik Zambrano <erikpintozambrano@outlook.com>
2017-01-11 21:36:40 -0600

    lesson 9 changes
    Add initial and end projects

---
+++ 09Lesson/finish/VisualStudioFeatures/src/VisualStudioFeatures/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VisualStudioFeatures.Models;
+
+namespace VisualStudioFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        Repository _repository = Repository.SharedRepository;
+
+        public IActionResult Index()
+        {
+            return View(Repository.SharedRepository.GetProducts);
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View(new Product());
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            _repository.AddProducts(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 09Lesson/finish/VisualStudioFeatures/test/VisualStudioFeatures.Tests/ProductTests.cs
@@ -0,0 +1,35 @@
+﻿using VisualStudioFeatures.Models;
+using Xunit;
+
+namespace VisualStudioFeatures.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+
+            // Arrange
+            var p = new Product { Name = "Test", Price = 100M };
+
+            // Act
+            p.Name = "New Name";
+
+            //Assert
+            Assert.Equal("New Name", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            // Arrange
+            var p = new Product { Name = "Test", Price = 100M };
+
+            // Act
+            p.Price = 200M;
+
+            //Assert
+            Assert.Equal(200M, p.Price);
+        }
+    }
+}
