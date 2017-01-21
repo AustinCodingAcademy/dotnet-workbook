a25493cc009e4909063d3a11941a6f5225a2f92e
Curt Arbtin <arbtin@gmail.com>
2017-01-20 19:39:49 -0600

    Week 1 Day In class project
    Empty MVC Project

---
+++ Examples/Wk1Day2_Empty/src/Wk1Day2_Empty/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Wk1Day2_Empty.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Wk1Day2_Empty.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            string[] myArray = new string[] { "ACA", "Dotnet", "Core" };
+            ViewData["my_arr"] = myArray;
+            // Looking in the Product model and you will find the GetProducts() method
+            // the GetProducts() method adds items statically to the Product domain model
+            // below i create a anonymous type and call the GetProducts() class method
+            var product = Product.GetProducts();
+            
+            return View(product);
+        }
+    }
+}
---
+++ Examples/Wk1Day2_Empty/src/Wk1Day2_Empty/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wk1Day2_Empty.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product
+            {
+                Name = "Kayak",
+                Price = 275M
+            };
+
+            Product lifejacket = new Product
+            {
+                Name = "Life Jacket",
+                Price = 48.95M
+            };
+
+            return new Product[] { kayak, lifejacket };
+        }
+    }
+}
