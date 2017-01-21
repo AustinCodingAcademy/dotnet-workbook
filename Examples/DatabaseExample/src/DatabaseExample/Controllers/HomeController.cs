4ad4551a03fba524c94595633a1ef1bbb3d5600c
Curt Arbtin <arbtin@gmail.com>
2017-01-21 03:09:06 -0600

    Database example
    Example of a live connection to Gearhost SQL instance

---
+++ Examples/DatabaseExample/src/DatabaseExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DatabaseExample.Data;
+
+namespace DatabaseExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Examples/DatabaseExample/src/DatabaseExample/Data/CharacterContext.cs
@@ -0,0 +1,17 @@
+﻿using DatabaseExample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseExample.Data
+{
+    public class CharacterContext : DbContext
+    {
+        public CharacterContext()
+        {
+        }
+
+        public CharacterContext(DbContextOptions<CharacterContext> options) : base(options)
+        {}
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
