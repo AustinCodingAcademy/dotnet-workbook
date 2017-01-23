12b96f11c05c0e8f266df1b64d8497a3cf03a911
Erik Zambrano <erikpintozambrano@outlook.com>
2017-01-22 22:20:17 -0600

    lesson 13 - updates

---
+++ 13Lesson/finish/SportsStore/src/SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System.Linq;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        private IOrderRepository _orderRepository;
+        private Cart cart;
+
+        public OrderController(IOrderRepository orderRepository, Cart cartService)
+        {
+            _orderRepository = orderRepository;
+            cart = cartService;
+        }
+
+        public ViewResult Checkout() => View(new Order());
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                _orderRepository.SaveOrder(order);
+                return RedirectToAction(nameof(Completed));
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+
+        public ViewResult Completed()
+        {
+            cart.Clear();
+            return View();
+        }
+
+        public ViewResult List() =>  View(_orderRepository.Orders.Where(o => !o.Shipped));
+
+        [HttpPost]
+        public IActionResult MarkShipped(int orderID)
+        {
+            Order order = _orderRepository.Orders
+                .FirstOrDefault(o => o.OrderID == orderID);
+            if (order != null)
+            {
+                order.Shipped = true;
+                _orderRepository.SaveOrder(order);
+            }
+            return RedirectToAction(nameof(List));
+        }
+    }
+}
---
+++ 13Lesson/finish/SportsStore/src/SportsStore/Models/OrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private ApplicationDbContext context;
+
+        public OrderRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IEnumerable<Order> Orders => context.Orders
+                            .Include(o => o.Lines)
+                            .ThenInclude(l => l.Product);
+
+        public void SaveOrder(Order order)
+        {
+            context.AttachRange(order.Lines.Select(l => l.Product));
+            if (order.OrderID == 0)
+            {
+                context.Orders.Add(order);
+            }
+            context.SaveChanges();
+        }
+    }
+}
