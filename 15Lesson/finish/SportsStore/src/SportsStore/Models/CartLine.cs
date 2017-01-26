3e2b232bf082096f49ac6cf43b0fc38e13ff5428
Erik Zambrano <erikpintozambrano@outlook.com>
2017-01-25 22:08:03 -0600

    Lesson 15 - Finish project

---
+++ 15Lesson/finish/SportsStore/src/SportsStore/Models/CartLine.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsStore.Models
+{
+    public class CartLine
+    {
+        public int CartLineID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ 15Lesson/finish/SportsStore/src/SportsStore/Startup.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SportsStore.Models;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        IConfigurationRoot _configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    _configuration["Data:SportsStoreProducts:ConnectionString"]));
+
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                options.UseSqlServer(_configuration["Data:SportsStoreIdentity:ConnectionString"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IOrderRepository, OrderRepository>();
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseIdentity();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "Error",
+                    template: "Error",
+                    defaults: new { controller = "Error", action = "Error" }
+                );
+
+                 routes.MapRoute(
+                    name: null,
+                    template: "{category}/Page/{page:int}",
+                    defaults: new { controller = "Product", action = "List" }
+                );
+
+                routes.MapRoute(
+                    name: null,
+                    template: "Page/{page:int}",
+                    defaults: new { controller = "Product", action = "List", page = 1 }
+                );
+
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}",
+                    defaults: new { controller = "Product", action = "List", page = 1 }
+                );
+
+                routes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new { controller = "Product", action = "List", page = 1 }
+                );
+
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
+            });
+
+            SeedData.EnsurePopulated(app);
+            IdentitySeedData.EnsurePopulated(app);
+        }
+    }
+}
