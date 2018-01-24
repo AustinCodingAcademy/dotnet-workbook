9986ea85b2659c9417b74dfecda694094e2dbf1f
Erik Zambrano <erik@austincodingacademy.com>
2018-01-23 20:52:49 -0600

    first MVC application

---
+++ v2/01week/Project1/Project1/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product[] products = Product.GetProducts();
+
+            return View(products);
+        }
+    }
+}
