4be4004b4c0a0430c596a5310d6b49e9e5975183
Maxfield Gaj <maxfieldegaj@gmail.com>
2018-02-01 12:54:46 -0600

    Finished pre-reading project

---
+++ V2/03week/WorkingWithVisualStudio/WorkingWithVisualStudio/Models/SImpleRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithVisualStudio.Models
+{
+    public class SimpleRepository
+    {
+        private static SimpleRepository sharedRepository = new SimpleRepository();
+        private Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+        public static SimpleRepository SharedRepository => sharedRepository;
+
+        public SimpleRepository()
+        {
+            var initialItems = new[]
+            {
+                new Product {Name = "Kayak", Price = 275m },
+                new Product {Name = "Lifejacket", Price = 48.95m },
+                new Product {Name = "Soccer ball", Price = 19.50m },
+                new Product {Name = "Corner Flag", Price = 34.95m }
+            };
+            foreach (var p in initialItems)
+            {
+                AddProduct(p);
+            }
+            products.Add("Error", null);
+        }
+
+        
+
+        public IEnumerable<Product> Products => products.Values;
+
+        public void AddProduct(Product p) => products.Add(p.Name, p);
+    }
+}
