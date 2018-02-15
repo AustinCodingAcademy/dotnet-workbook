60735e046cc0fb970cfd95d2efa4c2dd3a302f90
unknown <JCDenton46@gmail.com>
2018-02-15 14:59:53 -0600

    Copied the Schools example with Grades. One of the projects got renamed and can't be found.

---
+++ v2/04week/Grades/Grades.Data/GradesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Grades.Data
+{
+    public class GradesDbContext : DbContext
+    {
+        public SchooolDbContext(DbContextOptions options) : base (options)
+        {
+
+        }
+
+        public DbSet<Session> Sessions {get; set;} 
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ v2/04week/Grades/Grades.WebUI/Services/SessionServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Grades.WebUI.Services
+{
+    public class SessionServices
+    {
+        private GradesDbContext _context;
+
+        public SessionServices(GradesDbContext context)
+        {
+            _context = context;
+        }
+
+        public Grades GetSingleGradeById(int Id)
+        {
+            return _context.Grades.Single(g => g.Id == id);
+        }
+
+        public List<Grades> GetAllGrades()
+        {
+            return _context.Grades.ToList();
+        }
+    }
+}
