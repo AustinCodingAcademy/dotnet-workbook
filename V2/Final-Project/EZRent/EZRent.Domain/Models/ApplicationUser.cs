dd6d3a13af549938fb3a5a84daa4b5c2f08f521e
Maxfield Gaj <maxfieldegaj@gmail.com>
2018-03-26 01:09:48 -0500

    Stylized home page, added users for guest login

--- V2/Final-Project/EZRent/EZRent.Domain/Models/ApplicationUser.cs
+++ V2/Final-Project/EZRent/EZRent.Domain/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EZRent.Domain.Models
@@ -8,7 +9,12 @@ namespace EZRent.Domain.Models
     public class ApplicationUser : IdentityUser
     {
         // properties
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
     }
 }
